d2fbf568410c1c866585769972c581280eb8f627
Alessandro Bernardi <alexbernardi360@gmail.com>
2020-10-23 22:36:28 +0200

    Several improvements in the code.

--- GBparser.js
+++ GBparser.js
@@ -1,35 +1,30 @@
-// Requires imported modules
-const path      = require('path')
-const sqlite3   = require('sqlite3').verbose()
+// Requires external module
+const path    = require('path')
 
 // Requires internal modules
-const lib       = require('./lib')
+const db    = require('./lib/aa-sqlite3')
+const lib   = require('./lib/GBlib')
 
-// Get the path of the SQLite file from command line arguments.
-var path_file = process.argv[2]
+async function main() {
+    // Get the path of the SQLite file from command line arguments.
+    const file_path = process.argv[2]
 
-if (!path.isAbsolute(path_file))
-    path_file = `./${path_file}`
+    // Get title of the GameBook
+    const title = path.basename(file_path, path.extname(file_path))
 
-var title = path.basename(path_file, path.extname(path_file))
+    // Connects to SQLite DB
+    console.log(await db.open(file_path))
 
-// Connects to SQLite DB
-let db = new sqlite3.Database(path_file, sqlite3.OPEN_READONLY, (error) => {
-    if (error)
-        console.error(error.message)
-    else
-        console.log(`Connected to the DB: ${title}.`)
-})
+    const GameBook  = {
+        "chapters": await lib.makeChapters(db),
+        "info":     await lib.makeInfo(db),
+        "intro":    await lib.makeIntro(db),
+        "rules":    await lib.makeRules(db)
+    }
 
-// Elaborates SQLite DB
-lib.getNumberOfChapters(db, (numChapters) => {
-    lib.SQLiteToJSON(db, numChapters, (chapters) => {
-        db.close((error) => {
-            if (error)
-                console.error.apply(error.message)
-            else
-                console.log('DB closed.')
-        })
-        lib.writeToJSON(chapters, title)
-    })
-})
+    console.log(await lib.exportToFile(GameBook, title))
+
+    db.close()
+}
+
+main()
