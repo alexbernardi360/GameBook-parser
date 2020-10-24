72878153cb5965b29db35cbd2804d9366aa76583
Alessandro Bernardi <alexbernardi360@gmail.com>
2020-10-24 18:28:42 +0200

    Bugfixes.

--- GBparser.js
+++ GBparser.js
@@ -9,22 +9,33 @@ async function main() {
     // Get the path of the SQLite file from command line arguments.
     const file_path = process.argv[2]
 
-    // Get title of the GameBook
-    const title = path.basename(file_path, path.extname(file_path))
+    if (file_path) {
+        // Get title of the GameBook
+        const title = path.basename(file_path, path.extname(file_path))
 
-    // Connects to SQLite DB
-    console.log(await db.open(file_path))
+        try {
+            // Connects to SQLite DB
+            console.log(await db.open(file_path))
 
-    const GameBook  = {
-        "chapters": await lib.makeChapters(db),
-        "info":     await lib.makeInfo(db),
-        "intro":    await lib.makeIntro(db),
-        "rules":    await lib.makeRules(db)
-    }
+            // Creation of object containing data
+            const GameBook  = {
+                "chapters": await lib.makeChapters(db),
+                "info":     await lib.makeInfo(db),
+                "intro":    await lib.makeIntro(db),
+                "rules":    await lib.makeRules(db)
+            }
 
-    console.log(await lib.exportToFile(GameBook, title))
+            // Exporting the object to file
+            console.log(await lib.exportToFile(GameBook, title))
 
-    db.close()
+            // Closing the DB
+            db.close()
+
+        } catch (error) {
+            console.log(error)
+        }
+    } else
+        console.log('Too few arguments: the path to the GameBook in SQLite3 format is missing.')
 }
 
 main()
